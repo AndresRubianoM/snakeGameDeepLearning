a2863390d476696138bceb9c9d0286713206f5ae
Andrés Rubiano <andresfrm2210@gmail.com>
2020-09-10 15:02:25 -0500

    Cleaning

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 from snakeGame.screen import Screen
 
+from learningModels.process import LearningProcess
+from learningModels.snake_AI import SnakeAI
+from learningModels.GameEnv import SnakeGameEnv
 
-game = Screen(300, 300, [15,15], 5)
-game.start_pygame()
-game.game_cycle()
+def training():
+    snake = SnakeAI([30,30])
+    agent_environment = SnakeGameEnv(snake)
+    Lp = LearningProcess(agent_environment)
+    Lp.pre_learning_process()
+    Lp.training() 
+    Lp.policy_saver()
+
+
+def playing_AI():
+    snake = SnakeAI([30,30])
+    agent_environment = SnakeGameEnv(snake)
+    screen = Screen(300, 300, [30, 30], 5)
+    Lp = LearningProcess(agent_environment)
+
+    Lp.play_previous_policy(screen)
+    
+
+
+
+
+if __name__ == "__main__":
+    training()
+    playing_AI()
+    
+
+    
