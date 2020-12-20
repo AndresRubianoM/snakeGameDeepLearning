02e55e325458eae9115363400da791898504138a
Andres Rubiano <andresfrm2210@gmail.com>
2020-12-20 14:26:29 -0500

    change of directory for snake_AI and snake_AI_borders

---
+++ learningModels/typesSnakeGame/__init__.py
@@ -0,0 +1,5 @@
+"""This module contain all the differents types of the snake game for training
+the network (Actually contains the snake game with and without borders).
+ """
+from .snake_AI import*
+from .snake_AI_borders import *
